659af54e656085007da145a352403eb99d416e14
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 03:09:10 +0000

    image optimization and test fixes

---
+++ tests/Admin/AdminTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\StockTaking\tests\Admin;
+
+/**
+ * @internal
+ */
+final class AdminTest extends \PHPUnit\Framework\TestCase
+{
+    protected const NAME = 'StockTaking';
+
+    protected const URI_LOAD = '';
+
+    use \tests\Modules\ModuleTestTrait;
+}
