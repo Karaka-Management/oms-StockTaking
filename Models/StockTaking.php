03c6d8bd1abac5ae3557ffa1e313b81ae420c4cd
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-04-25 01:01:37 +0000

    fix tests

--- Models/StockTaking.php
+++ Models/StockTaking.php
@@ -38,6 +38,11 @@ final class StockTaking
 
     public int $unit = 0;
 
+    /**
+     * Constructor.
+     *
+     * @since 1.0.0
+     */
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable();
