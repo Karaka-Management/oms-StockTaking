3131e069664dab02d8baf5bb433688e63e6eae12
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:18 +0200

    fix version and bugs

--- Models/StockTakingMapper.php
+++ Models/StockTakingMapper.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\WarehouseManagement\Models
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -24,7 +24,7 @@ use phpOMS\DataStorage\Database\Mapper\DataMapperFactory;
  * StockTaking mapper class.
  *
  * @package Modules\StockTaking\Models
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  *
